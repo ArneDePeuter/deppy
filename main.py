d89e7cdef51cf37cb75e38e115043b17877eb8ce
ArneDePeuter <107651037+ArneDePeuter@users.noreply.github.com>
2025-01-09 21:02:46 +0100

    CHORE: main.py

--- main.py
+++ main.py
@@ -1,33 +1,41 @@
 import asyncio
-from deppy import Deppy
+from deppy.deppy import Deppy
+from deppy.cache import Cache
+import json
 
-# Instantiate the dependency resolver
-deppy = Deppy()
 
-@deppy.node
-async def a():
-    print("Executing Node A")
+async def fetch_data():
+    print("Fetching data...")
     await asyncio.sleep(1)
-    return 2
-
-@deppy.node
-def b():
-    print("Executing Node B")
     return [1, 2, 3]
 
-@deppy.node
-async def c(val1, val2):
-    print(f"Executing Node C with val1={val1} and val2={val2}")
-    await asyncio.sleep(0.5)
-    return val1 * val2
 
-# Add loop variables
-c.val1(a)
-c.val2(b, loop=True)
+async def process_data(data):
+    await asyncio.sleep(1)
+    return [x * 2 for x in data]
+
+
+async def combine_data(item, multiplier=2):
+    await asyncio.sleep(0.5)
+    return item * multiplier
+
+deppy = Deppy()
+
+fetch_data_node = deppy.node(fetch_data, cache=Cache(max_uses=1, ttl=10))
+process_data_node = deppy.node(process_data)
+combine_data_node = deppy.node(combine_data)
+
+process_data_node.data(fetch_data_node)
+combine_data_node.item(process_data_node, loop=True)
+
 
 async def main():
-    print("Starting Execution")
-    results = await deppy.execute()
-    print("Execution Results:", results)
+    print("Executing dependency graph...")
+    result = await deppy.execute()
+    print("Graph execution complete!")
+    print("Result:")
+    for node, value in result.items():
+        print(f"\t {node}: {json.dumps(value)}")
 
-asyncio.run(main())
+if __name__ == "__main__":
+    asyncio.run(main())
