c406190612f3180deb1ff320ce54313610c5b024
ArneDePeuter <107651037+ArneDePeuter@users.noreply.github.com>
2025-01-11 20:23:51 +0100

    CHORE: cleanup

--- deppy/wrappers/__init__.py
+++ deppy/wrappers/__init__.py
@@ -1,1 +1,6 @@
 from .dkr import Dkr, MappingDk, StringDk, JsonDk, IterDk
+# recommended libraries/objects for wrappers
+# caching:
+# from functools import cache, lru_cache
+# retrying:
+# import tenacity
