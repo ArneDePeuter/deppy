ad35871917a26df26096c33329dcca159c6ac076
ArneDePeuter <107651037+ArneDePeuter@users.noreply.github.com>
2025-01-11 13:43:29 +0100

    FEAT: result dot graph

--- deppy/scope.py
+++ deppy/scope.py
@@ -43,3 +43,22 @@ class Scope(dict):
 
     def __hash__(self) -> int:
         return id(self)
+
+    def dot(self, filename: str) -> None:
+        import pydot
+
+        graph = pydot.Dot(graph_type='digraph')
+
+        def add_node(scope):
+            label = ""
+            for key, value in scope.items():
+                label += f"{key}: {value}\n"
+            node = pydot.Node(id(scope), label=label)
+            graph.add_node(node)
+            for child in scope.children:
+                child_node = add_node(child)
+                graph.add_edge(pydot.Edge(node, child_node))
+            return node
+
+        add_node(self)
+        graph.write(filename)
