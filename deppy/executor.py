3654faa9ea0428ac2461abf4d60316508b2bf50f
ArneDePeuter <107651037+ArneDePeuter@users.noreply.github.com>
2025-01-11 05:09:46 +0100

    FEAT: target nodes

--- deppy/executor.py
+++ deppy/executor.py
@@ -1,5 +1,5 @@
 import asyncio
-from typing import Dict, Any, List
+from typing import Dict, Any, List, Sequence
 from networkx import MultiDiGraph
 
 from .node import Node
@@ -11,6 +11,7 @@ class Executor:
         self.graph = graph
         self.root = Scope()
         self.work_graph = self.graph.copy()
+        self.flow_graph = self.graph.copy()
 
     async def execute_node(self, node: Node, scope: Scope) -> None:
         args_list = await self._resolve_args(node, scope)
@@ -28,15 +29,31 @@ class Executor:
                 new_child[node] = result
                 scopes.add(new_child)
 
-        successors = self.graph.successors(node)
+        successors = self.flow_graph.successors(node)
         if node in self.work_graph:
             self.work_graph.remove_node(node)
         qualified_nodes = [successor for successor in successors if self.work_graph.in_degree(successor) == 0]
         await asyncio.gather(*[self.execute_node(successor, scope) for successor in qualified_nodes for scope in scopes])
 
-    async def execute(self) -> Scope:
+    def subgraph_work_graph(self, *target_nodes):
+        relevant_nodes = set()
+        new_nodes = set(target_nodes)
+        while new_nodes:
+            relevant_nodes.update(new_nodes)
+            newer = set()
+            for node in new_nodes:
+                newer.update(self.work_graph.predecessors(node))
+            new_nodes = newer
+        irrelevant_nodes = set(self.work_graph) - relevant_nodes
+        self.work_graph.remove_nodes_from(irrelevant_nodes)
+
+    async def execute(self, *target_nodes: Sequence[Node]) -> Scope:
         """Execute the graph layer by layer."""
         self.work_graph = self.graph.copy()
+        if len(target_nodes) > 0:
+            self.subgraph_work_graph(*target_nodes)
+        self.flow_graph = self.work_graph.copy()
+
         qualified_nodes = [node for node in self.work_graph if self.work_graph.in_degree(node) == 0]
         await asyncio.gather(*[self.execute_node(node, self.root) for node in qualified_nodes])
         return self.root
