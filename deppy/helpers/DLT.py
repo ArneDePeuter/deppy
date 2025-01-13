1d781a089cc28e0f1d0632167eadda2b11cd67e5
ArneDePeuter <107651037+ArneDePeuter@users.noreply.github.com>
2025-01-13 13:25:32 +0100

    FEAT: improve dlt functionality

--- deppy/helpers/DLT.py
+++ deppy/helpers/DLT.py
@@ -29,61 +29,47 @@ def create_spec(source_name: str, configs: Set[str], secrets: Set[str]) -> BaseC
 
 
 def deppy_to_source(
-        deppy: Type[Deppy],
-        target_nodes: Optional[Iterable[str]] = None,
-        secrets: Optional[Iterable[str]] = None,
-        exclude_for_storing: Optional[Iterable[str]] = None,
+        deppy: Deppy,
+        target_nodes: Optional[Iterable[Node]] = None,
+        exclude_for_storing: Optional[Iterable[Node]] = None,
         with_pbar: Optional[bool] = False
 ) -> DltSource:
-    name = str(deppy.__name__).lower()
+    name = deppy.name.lower()
     target_nodes = target_nodes or []
-    secrets = set(secrets) or set()
     exclude_for_storing = exclude_for_storing or []
+    secrets = set(deppy.graph_builder.secrets.keys())
 
-    init_param_names = set(inspect.signature(deppy.__init__).parameters.keys())
-    init_param_names.remove("self")
-    configs = init_param_names - secrets
+    configure_param_names = set(inspect.signature(deppy.configure).parameters.keys())
+    configure_param_names.remove("kwargs")
+    configs = configure_param_names | set(deppy.graph_builder.consts.keys())
     spec = create_spec(name, configs, secrets)
 
     @dlt.source(name=f"{name}_source")
     def source():
         resolved_spec = resolve_configuration(spec(), sections=("sources", name))  # type: ignore[operator]
-        init_kwargs = {k: getattr(resolved_spec, k) for k in init_param_names}
-        deppy_object = deppy(**init_kwargs)
-
-        actual_target_nodes = set()
-        for n in target_nodes:
-            node = deppy_object.get_node_by_name(n)
-            if node is None:
-                raise ValueError(f"Node {n} not found in {deppy_object}")
-            actual_target_nodes.add(node)
+        configure_kwargs = {k: getattr(resolved_spec, k) for k in (configs | secrets)}
+        deppy.configure(**configure_kwargs)
 
         @dlt.resource(selected=False, name=f"{name}_extract")
         async def extract() -> DltResource:
-            func = deppy_object.execute(*actual_target_nodes, with_pbar=with_pbar)
+            func = deppy.execute(*target_nodes, with_pbar=with_pbar)
             if hasattr(deppy, "__aenter__"):
-                async with deppy_object:
+                async with deppy:
                     yield await func
             elif hasattr(deppy, "__enter__"):
-                with deppy_object:
+                with deppy:
                     yield await func
             else:
                 yield await func
 
         resources = [extract]
-        nodes: list[Node] = deppy_object.graph.nodes if len(target_nodes) == 0 else target_nodes
-
-        actual_exclude_for_storing = set()
-        for n in exclude_for_storing:
-            node = deppy_object.get_node_by_name(n)
-            if node is None:
-                raise ValueError(f"Node {n} not found in {deppy_object}")
-            actual_exclude_for_storing.add(node)
-
-        nodes = [node for node in nodes if node not in actual_exclude_for_storing]
+        nodes: list[Node] = deppy.graph.nodes if len(target_nodes) == 0 else target_nodes
+        nodes = [node for node in nodes if node not in exclude_for_storing]
 
         for n in nodes:
             if n.secret:
+                continue
+            if n.name in deppy.consts:
                 continue
 
             @dlt.transformer(data_from=extract, name=n.name)
