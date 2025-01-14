b3c771fc1f91c567bee2d5a1d593f067d2d107a5
ArneDePeuter <107651037+ArneDePeuter@users.noreply.github.com>
2025-01-14 23:10:59 +0100

    FEAT: hybrid executor

--- deppy/executor/async_executor.py
+++ deppy/executor/async_executor.py
@@ -10,17 +10,9 @@ class AsyncExecutor(Executor):
     def __init__(self, deppy) -> None:
         super().__init__(deppy)
 
-    @staticmethod
-    async def call_node(node: Node, **kwargs):
-        if node.is_async:
-            return await node.call_async(**kwargs)
-        if node.to_thread:
-            return await asyncio.to_thread(node.call_sync, **kwargs)
-        return node.call_sync(**kwargs)
-
     async def execute_node_with_scope_async(self, node: Node, scope: Scope) -> Set[Scope]:
         call_args = self.resolve_args(node, scope)
-        results = await asyncio.gather(*[self.call_node(node, **args) for args in call_args])
+        results = await asyncio.gather(*[node.call_async(**args) for args in call_args])
         return self.save_results(node, list(results), scope)
 
     async def execute_node_async(self, node: Node) -> None:
