3a57ab2aad5e41e17edcb317a84c236fcdd8e276
ArneDePeuter <107651037+ArneDePeuter@users.noreply.github.com>
2025-01-15 10:37:48 +0100

    FEAT: multithreading

--- deppy/executor/sync_executor.py
+++ deppy/executor/sync_executor.py
@@ -1,4 +1,7 @@
-from typing import Sequence, Set
+from typing import Sequence, Set, Dict, Tuple
+from concurrent.futures import ThreadPoolExecutor, as_completed, Future
+from typing import Optional
+
 
 from deppy.node import Node
 from deppy.scope import Scope
@@ -6,9 +9,12 @@ from .executor import Executor
 
 
 class SyncExecutor(Executor):
-    # TODO: add threading
-    def __init__(self, deppy) -> None:
+    def __init__(self, deppy, max_thread_workers: Optional[int] = None, *args, **kwargs) -> None:
         super().__init__(deppy)
+        self.thread_pool = ThreadPoolExecutor(max_workers=max_thread_workers)
+
+    def shutdown(self):
+        self.thread_pool.shutdown()
 
     def execute_node_with_scope_sync(self, node: Node, scope: Scope) -> Set[Scope]:
         call_args = self.resolve_args(node, scope)
@@ -21,6 +27,39 @@ class SyncExecutor(Executor):
         self.scope_map[node] = set.union(*new_scopes)
         self.mark_complete(node)
 
+    def gather_thread_tasks(self, node: Node) -> Dict[Future, Tuple[Node, Scope]]:
+        task_map = {}
+        for scope in self.get_call_scopes(node):
+            call_args = self.resolve_args(node, scope)
+            for args in call_args:
+                task = self.thread_pool.submit(node.call_sync, **args)
+                task_map[task] = (node, scope)
+        return task_map
+
+    def execute_threaded_nodes(self, nodes: Set[Node]):
+        task_map = {}
+        for node in nodes:
+            task_map.update(self.gather_thread_tasks(node))
+
+        for future in as_completed(task_map):
+            result = future.result()
+            node, scope = task_map[future]
+            if node not in self.scope_map:
+                self.scope_map[node] = set()
+            new_scopes = self.save_results(node, [result], scope)
+            self.scope_map[node].update(new_scopes)
+
+        for node in nodes:
+            self.mark_complete(node)
+
+    def execute_nodes_sync(self, nodes: Set[Node]) -> None:
+        threaded_nodes = {node for node in nodes if node.to_thread}
+        sync_nodes = nodes - threaded_nodes
+
+        self.execute_threaded_nodes(threaded_nodes)
+        for node in sync_nodes:
+            self.execute_node_sync(node)
+
     def execute_sync(self, *target_nodes: Sequence[Node]) -> Scope:
         self.setup(*target_nodes)
         ready_nodes = self.get_ready_nodes()
@@ -30,8 +69,7 @@ class SyncExecutor(Executor):
             current_tasks = tasks
             tasks = set()
 
-            for node in current_tasks:
-                self.execute_node_sync(node)
+            self.execute_nodes_sync(current_tasks)
 
             for node in current_tasks:
                 successors = self.qualified_successors(node)
