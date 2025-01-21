18c0d463216ab772638507b0dbfc9af74ec2a6b7
ArneDePeuter <107651037+ArneDePeuter@users.noreply.github.com>
2025-01-21 13:24:50 +0100

    CHORE: document sync/async/hybrid executor

--- deppy/executor/sync_executor.py
+++ deppy/executor/sync_executor.py
@@ -2,28 +2,72 @@ from typing import Sequence, Set, Dict, Tuple
 from concurrent.futures import ThreadPoolExecutor, as_completed, Future
 from typing import Optional
 
-
 from deppy.node import Node
 from deppy.scope import Scope
 from .executor import Executor
 
 
 class SyncExecutor(Executor):
+    """
+    A synchronous executor for executing nodes in dependency graphs.
+
+    Attributes
+    ----------
+    thread_pool : ThreadPoolExecutor
+        Thread pool used for parallel execution of nodes.
+    """
+
     def __init__(
         self, deppy, max_thread_workers: Optional[int] = None, *args, **kwargs
     ) -> None:
+        """
+        Constructs a SyncExecutor instance.
+
+        Parameters
+        ----------
+        deppy : Any
+            The main dependency manager instance.
+        max_thread_workers : Optional[int], optional
+            Maximum number of threads in the thread pool (default is None, which allows unlimited threads).
+        """
         super().__init__(deppy)
         self.thread_pool = ThreadPoolExecutor(max_workers=max_thread_workers)
 
     def shutdown(self):
+        """
+        Shuts down the thread pool executor, releasing resources.
+        """
         self.thread_pool.shutdown()
 
     def execute_node_with_scope_sync(self, node: Node, scope: Scope) -> Set[Scope]:
+        """
+        Executes a single node synchronously within a given scope.
+
+        Parameters
+        ----------
+        node : Node
+            The node to execute.
+        scope : Scope
+            The scope in which the node is executed.
+
+        Returns
+        -------
+        Set[Scope]
+            A set of scopes resulting from the node's execution.
+        """
         call_args = self.resolve_args(node, scope)
         results = [node.call_sync(**args) for args in call_args]
         return self.save_results(node, list(results), scope)
 
     def execute_node_sync(self, node: Node) -> None:
+        """
+        Executes a single node synchronously across all its applicable scopes.
+
+        Parameters
+        ----------
+        node : Node
+            The node to execute.
+        """
         scopes = self.get_call_scopes(node)
         if len(scopes) == 0:
             return
@@ -33,6 +77,19 @@ class SyncExecutor(Executor):
         self.scope_map[node] = set.union(*new_scopes)
 
     def gather_thread_tasks(self, node: Node) -> Dict[Future, Tuple[Node, Scope]]:
+        """
+        Gathers thread pool tasks for a threaded node's execution.
+
+        Parameters
+        ----------
+        node : Node
+            The node for which to gather tasks.
+
+        Returns
+        -------
+        Dict[Future, Tuple[Node, Scope]]
+            A mapping of Future objects to their corresponding node and scope.
+        """
         task_map = {}
         for scope in self.get_call_scopes(node):
             call_args = self.resolve_args(node, scope)
@@ -42,6 +99,14 @@ class SyncExecutor(Executor):
         return task_map
 
     def execute_threaded_nodes(self, nodes: Set[Node]):
+        """
+        Executes threaded nodes using the thread pool.
+
+        Parameters
+        ----------
+        nodes : Set[Node]
+            The set of nodes to execute using threads.
+        """
         task_map = {}
         for node in nodes:
             task_map.update(self.gather_thread_tasks(node))
@@ -55,6 +120,14 @@ class SyncExecutor(Executor):
             self.scope_map[node].update(new_scopes)
 
     def execute_nodes_sync(self, nodes: Set[Node]) -> None:
+        """
+        Executes a set of nodes, either synchronously or using threads based on their properties.
+
+        Parameters
+        ----------
+        nodes : Set[Node]
+            The set of nodes to execute.
+        """
         threaded_nodes = {node for node in nodes if node.to_thread}
         sync_nodes = nodes - threaded_nodes
 
@@ -63,6 +136,19 @@ class SyncExecutor(Executor):
             self.execute_node_sync(node)
 
     def execute_sync(self, *target_nodes: Sequence[Node]) -> Scope:
+        """
+        Executes the dependency graph synchronously.
+
+        Parameters
+        ----------
+        target_nodes : Sequence[Node]
+            The target nodes to execute.
+
+        Returns
+        -------
+        Scope
+            The root scope containing the execution results.
+        """
         self.setup(*target_nodes)
 
         for tasks in self.batched_topological_order():
